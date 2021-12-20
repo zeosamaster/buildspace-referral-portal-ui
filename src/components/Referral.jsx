4a6239fa8729c8e2dd4c989cdc62c8f2715dab83
Fábio Matias <fabio_mcm2@hotmail.com>
2021-12-20 00:08:06 +0000

    feat: add react-hook-form

--- src/components/Referral.jsx
+++ src/components/Referral.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 export function Referral({ account }) {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = React.useCallback(({ address }) => {}, []);
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="address">
         <span className="address-text">
           Add an address of a friend you want to praise
         </span>
-        <input className="address-input" />
+        <input className="address-input" {...register("address")} />
       </div>
 
-      <button className="cta-button refer-button">Add Referral</button>
-    </>
+      <button className="cta-button refer-button" type="submit">
+        Add Referral
+      </button>
+    </form>
   );
 }
